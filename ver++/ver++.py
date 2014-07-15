ff914fe548fa77336174b25f60366bb5f5b9fc6f
oglop <fucker@fuck.com>
2014-07-16 00:10:27 +0800

    make it launchable from deadline monitor

--- ver++/ver++.py
+++ ver++/ver++.py
@@ -6,13 +6,14 @@ from System.IO import *
 from System.Diagnostics import *
 from Deadline.Scripting import *
 
-
+import sys
+import os
+import platform
 ########################################################################
 ## Globals
 ########################################################################
 script_dialog = None
 settings = None
-mayaVerDict = None
 
 ########################################################################
 ## Main Function Called By Deadline
@@ -21,42 +22,15 @@ def __main__():
 	global script_dialog
 	global settings
 	
-	ClientUtils.LogText(','.join(dir(ProcessUtils)))
-	# ProcessUtils.ShellExecute(r'D:\Dropbox\pyqt_learn\boxUI\tree_button_1.pyw')
-	# ClientUtils.ExecuteScript(r'D:\Dropbox\pyqt_learn\boxUI\tree_button.py')
-	# ProcessUtils.SpawnProcess(r'D:\Dropbox\pyqt_learn\boxUI\tree_button.pyw',r'',r'c:',ProcessWindowStyle.Hidden,False)
-	# ProcessUtils.SpawnProcess(r'D:\Dropbox\pyqt_learn\boxUI\tree_button.pyw',r'')
-
-	ClientUtils.LogText('yeah === ')
 	ClientUtils.LogText(GetRootDirectory())
 	ClientUtils.LogText(Path.Combine( GetRootDirectory(), r"scripts\General\ver++\dp_ver++.pyw"))
-	# ProcessUtils.SpawnProcess(Path.Combine( GetRootDirectory(), r"scripts\General\ver++\dp_ver++.pyw" ),r'')
-	# ProcessUtils.ShellExecute(Path.Combine( GetRootDirectory(), r"scripts\General\ver++\dp_ver++.pyw" ))
 	
-	import sys
-	sys.path.insert(0,r'\\oglop-fujitsu\DeadlineRepository\scripts\General\ver++')
-	import os
-	# os.chdir(r'c:\DeadlineRepository\scripts\General\ver++')
-	os.chdir(r'\\oglop-fujitsu\DeadlineRepository\scripts\General\ver++')
-	# import subprocess
-	ClientUtils.LogText('yeah ===  after')
-	# ProcessUtils.ShellExecute(r'\\oglop-fujitsu\DeadlineRepository\scripts\General\ver++\dp_ver++.pyw')
-	# ProcessUtils.SpawnProcess(r'\\oglop-fujitsu\DeadlineRepository\scripts\General\ver++\dp_ver++.pyw',r'\\oglop-fujitsu\DeadlineRepository')
-	
-	# ProcessUtils.ShellExecute(r'c:\DeadlineRepository\scripts\General\ver++\dp_ver++.pyw')
-	# ProcessUtils.ShellExecute(r'D:\Dropbox\pyqt_learn\DeadlineRepository\scripts\General\ver++\dp_ver++.pyw')
+	script_path = r"scripts/General/ver++/dp_ver++.pyw"
+	if platform.system()=='Windows':
+		script_path = os.path.normpath(script_path)
 
-	# ProcessUtils.SpawnProcess(r'D:\Dropbox\pyqt_learn\DeadlineRepository\scripts\General\ver++\dp_ver++.pyw',r'',r'c:',ProcessWindowStyle.Normal,False)
-
-	# ProcessUtils.SpawnProcess(r'D:\Dropbox\pyqt_learn\DeadlineRepository\scripts\General\ver++\dp_ver++.pyw')
-
-	# ProcessUtils.ShellExecute(r'python \\oglop-fujitsu\DeadlineRepository\scripts\General\ver++\dp_ver++.pyw')
-	# ProcessUtils.ShellExecute(r'\\oglop-fujitsu\DeadlineRepository\scripts\General\ver++\dp_ver++.pyw',r'\\oglop-fujitsu\DeadlineRepository')
-	# try:
-	# 	ProcessUtils.SpawnProcess(r'\\oglop-fujitsu\DeadlineRepository\scripts\General\ver++\dp_ver++.pyw',r'xxx')
-
-	# except Exception as e:
-	# 	ClientUtils.LogText('error')
-	# 	ClientUtils.LogText(str(e))
-
-	ProcessUtils.ShellExecute(r'\\oglop-fujitsu\DeadlineRepository\scripts\General\ver++\dp_ver++.pyw')
+	script_path = os.path.join(os.path.normpath(GetRootDirectory()),script_path)
+	ClientUtils.LogText(script_path)
+	script_path_root = os.path.dirname(script_path)
+	os.chdir(script_path_root)
+	ProcessUtils.ShellExecute(script_path)
